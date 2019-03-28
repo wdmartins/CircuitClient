5bc42c4cc9012ba798c0f553c9e34e45a7262756
Walter Martins <wdmartins@gmail.com>
2019-03-28 15:34:40 -0400

    Update CircuitDemo to report Temperature

--- examples/CircuitDemo/CircuitDemo.ino
+++ examples/CircuitDemo/CircuitDemo.ino
@@ -5,19 +5,41 @@
 #include <WiFiManager.h>
 #include <CircuitClient.h>
 #include <Ticker.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 // Access point to configure Wi-Fi
 #define ACCESS_POINT_NAME "ESP8266"
 #define ACCESS_POINT_PASS "esp8266"
 
-Ticker ticker;
+// GPIO Definitios
+#define DHT11_GPIO 2
+
+// Sensors Definitions
+#define DHT_TYPE DHT11
+#define DHT_TEMP_CHANGE_TRIGGER_CELSIUS 1
+#define DHT_HUMI_CHANGE_TRIGGER_PERCENT 1
+
+// Sensors Instantiation
+DHT_Unified dht(DHT11_GPIO, DHT_TYPE);
+
+// Intervals
+Ticker ledTicker;
+Ticker dhtTicker;
+
 // Circuit Client declaration
 CircuitClient *circuitClient;
 
 // Setup Liquid crystal display
 LiquidCrystal_I2C lcd(0x27, 16 ,2);
 
-void tick() {
+uint32_t dht11DelayMS;
+float currentTemperature = -300;
+float currentHumidity= -1;
+boolean updateDht11 = false;
+
+void ledTick() {
   int state = digitalRead(BUILTIN_LED);
   digitalWrite(BUILTIN_LED, !state);
 }
@@ -27,15 +49,62 @@ void configModeCallback (WiFiManager *myWiFiManager) {
   Serial.println("Entered config mode");
   Serial.println(WiFi.softAPIP());
   Serial.println(myWiFiManager->getConfigPortalSSID());
-  ticker.attach(0.2, tick);
+  ledTicker.attach(0.2, ledTick);
 }
 
 // Circuit new text item callback
 void onNewTextItemCB (String text) {
-  Serial.println("Text Received: ");
-  Serial.println(text);
+  Serial.print("Text Received: "); Serial.println(text);
   lcd.clear();
   lcd.print(text.c_str());
+}
+
+void setUpdateDht11() {
+  updateDht11 = !updateDht11;
+}
+
+void processDhtInfo () {
+  boolean report = false;
+  sensors_event_t event;
+  float temp;
+  float humidity;
+
+  dht.temperature().getEvent(&event);
+
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+    return;
+  } else {
+    temp = event.temperature;
+  }
+
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+    return;
+  } else {
+    humidity = event.relative_humidity;
+  }
+
+  if ((currentTemperature + DHT_TEMP_CHANGE_TRIGGER_CELSIUS) < temp ||
+    (currentTemperature - DHT_TEMP_CHANGE_TRIGGER_CELSIUS) > temp) {
+      report = true;
+  }
+  if (report == false && (((currentHumidity + DHT_HUMI_CHANGE_TRIGGER_PERCENT) < humidity) ||
+    ((currentHumidity - DHT_HUMI_CHANGE_TRIGGER_PERCENT > humidity)))) {
+      report = true;
+  }
+  currentTemperature = temp;
+  currentHumidity = humidity;
+  if (report) {
+    Serial.print("Reporting temperature: "); Serial.print(temp); Serial.print(" and humidity: "); Serial.println(currentHumidity);
+    char temp[17];
+    sprintf(temp, "Temp %d, Hum %d", static_cast<int>(currentTemperature), static_cast<int>(currentHumidity));
+    lcd.setCursor(0,1);
+    lcd.clear();
+    lcd.print(temp);
+    circuitClient->postTextMessage(temp);
+  }
 }
 
 void setup() {
@@ -48,7 +117,7 @@ void setup() {
 
   // Setup built in LED
   pinMode(BUILTIN_LED, OUTPUT);
-  ticker.attach(0.6, tick);
+  ledTicker.attach(0.6, ledTick);
   
   // Instantiate and setup WiFiManager
   WiFiManager wifiManager;
@@ -59,7 +128,7 @@ void setup() {
     delay(1000);  
   }
   Serial.println("Connection Sucessful");
-  ticker.detach();
+  ledTicker.detach();
   digitalWrite(BUILTIN_LED, LOW);
 
   // Configure Circuit Client library
@@ -75,10 +144,22 @@ void setup() {
   lcd.backlight();
   lcd.print("Ready!");
 
+  // Initialize DTH11 Sensor
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  dht11DelayMS = sensor.min_delay / 1000;
+  Serial.print(F("Sensor Minimun Delay:")); Serial.print(dht11DelayMS); Serial.println(F(" MS"));
+  dhtTicker.attach(dht11DelayMS / 1000, setUpdateDht11);
   
 }
 
 void loop() {
   // Allow Circuit client to run
   circuitClient->run();
+
+  // Check temperature
+  if (updateDht11) {
+    processDhtInfo();
+  }
 }
